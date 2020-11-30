5bff2844f2dc79348e73ded7f390ac15fcb86651
Mohammad Shohel <mohammadshohel866@gmail.com>
2020-11-30 20:29:34 +0600

    Successful Form Redirect.

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 
 from .forms import TweetForm
@@ -14,9 +14,15 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    # print("next_url", next_url)
     if form.is_valid():
         obj = form.save(commit=False)
+        # other form related logic
         obj.save()
+        if next_url != None:
+            return redirect(next_url)
+
         form = TweetForm()
     return render(request, "components/form.html", context={"form": form})
 

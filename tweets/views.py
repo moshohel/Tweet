8a6bef1e617d0d35d817ee1da93e8a7246450a00
Mohammad Shohel <mohammadshohel866@gmail.com>
2020-12-02 20:38:07 +0600

    Handling Ajax Requests.

--- tweets/views.py
+++ tweets/views.py
@@ -17,6 +17,7 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    print("ajax", request.is_ajax())
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     # print("next_url", next_url)
@@ -24,6 +25,8 @@ def tweet_create_view(request, *args, **kwargs):
         obj = form.save(commit=False)
         # other form related logic
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
 
